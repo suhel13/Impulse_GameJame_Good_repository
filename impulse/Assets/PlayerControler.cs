c356e68c1db9a8890d582a3576796c798b8e4263
suhel13 <39781998+suhel13@users.noreply.github.com>
2018-06-04 01:41:01 +0200

    first steps
    movement
    camera follow

---
+++ impulse/Assets/PlayerControler.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControler : MonoBehaviour
+{
+    Rigidbody rb;
+    [Range(5f,15f)] public float speed;
+    Vector3 axis;
+    // Use this for initialization
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        axis.x = Input.GetAxis("Horizontal");
+        axis.z = Input.GetAxis("Vertical");
+        axis.Normalize();
+        rb.velocity = axis * speed;
+
+    }
+}
