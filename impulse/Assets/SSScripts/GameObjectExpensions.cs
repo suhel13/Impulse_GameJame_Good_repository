3ad8a22306af03f8588d07b2471393d9bbf23720
suhel13 <39781998+suhel13@users.noreply.github.com>
2018-06-06 19:30:07 +0200

    exitZone and ...

---
+++ impulse/Assets/SSScripts/GameObjectExpensions.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class GameObjectExpensions
+{
+    public static Vector3 Pozyjca (this GameObject gameObject)
+    {
+        return gameObject.transform.position;
+    }
+}
