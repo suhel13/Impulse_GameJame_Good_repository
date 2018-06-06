54f1c5af4a05de257c2e2f31571ad9e5da4d4af9
suhel13 <39781998+suhel13@users.noreply.github.com>
2018-06-06 16:37:13 +0200

    nevMesh
    work perfecto

--- impulse/Assets/SSScripts/Nps.cs
+++ impulse/Assets/SSScripts/Nps.cs
@@ -34,7 +34,7 @@ public class Nps : MonoBehaviour
         {
             transform.LookAt(player.transform);
             destynation = player.transform.position;
-            agent.destination = destynation;
+            agent.SetDestination(destynation);
             if (Vector3.Distance(player.transform.position, transform.position) >= 7)
             {
                 followPlayer = false;
